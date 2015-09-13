2bc96202737a85898e3604fcb5603196bb1c92c0
narasimhaugra <narasimhaugra@gmail.com>
2015-09-13 16:29:53 +0530

    Fix Merge conflict

--- includes/cell.hpp
+++ includes/cell.hpp
@@ -5,6 +5,7 @@
 #ifndef  CELL_CLASS
 #define  CELL_CLASS
 
+
 class cBattery;
 #include <mutex>
 
@@ -29,22 +30,22 @@ class cCell
 		bool update(cBattery* owner,bool connected, double scurrent, double runtime);
 		bool loadDefaults(cBattery* owner);
 	private:
-		bool Locked;				
+		bool Locked;			
 		cBattery* AttachedTo;		
 		double InitialVoltage;		
 		double SeriesResistance;	
-		double Capacity;		
-		double Shift1;			
-		double Shift2;			
-		double Drop1;			
-		double Drop2;			
-		double Gradient;		
-		double ConstantK;		
+		double Capacity;			
+		double Shift1;				
+		double Shift2;				
+		double Drop1;				
+		double Drop2;				
+		double Gradient;			
+		double ConstantK;			
 		double m1,m2,m3;		
-		double DischargedCapacity;  
-		double RemainigCapacity;	
-		double SourceCurrent;		
-		double CurrentVoltage;		
+		double DischargedCapacity;
+		double RemainigCapacity;
+		double SourceCurrent;	
+		double CurrentVoltage;	
 		void initialise(void);
 		std::mutex AccessSynchroniser;
 };
